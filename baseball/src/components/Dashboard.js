3f1bb62a59b733e06400e3f19100fe96090f7ac9
matt0418 <mfeldma7@gmail.com>
2019-03-12 14:47:14 -0400

    Setting up project

---
+++ baseball/src/components/Dashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Dashboard extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            balls: 0,
+            strikes: 0
+        }
+    }
+
+    render() {
+        return(
+            <div>
+                Testing
+            </div>
+        )
+    }
+}
+
+export default Dashboard
